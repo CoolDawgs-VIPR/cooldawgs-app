73d4b70243f0fe32372166c975138f41f4b738e7
Kristian Pitshugin <kp39596@uga.edu>
2025-09-25 23:26:04 -0400

    Added Temperature + Graph
    Added Temperature + Graph to pet ID

--- app/petprofile/[id].tsx
+++ app/petprofile/[id].tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import BluetoothComponent from "../component/BluetoothComponent";
+import Graph from "../component/Graph";
+import Temperature from "../component/Temperature";
 
 export default function PetProfileScreen() {
-    const [status, setStatus] = useState<Boolean>(false);
-    const [connectedDevice, setConnectedDevice] = useState<string>("");
-    return (
-        <div>
-            <BluetoothComponent status={status} connectedDevice={connectedDevice} />
-        </div>
-    );
-
+  const [status, setStatus] = useState<Boolean>(false);
+  const [connectedDevice, setConnectedDevice] = useState<string>("");
+  return (
+    <div>
+      <BluetoothComponent status={status} connectedDevice={connectedDevice} />
+      <Temperature status={status} connectedDevice={connectedDevice} />
+      <Graph status={status} connectedDevice={connectedDevice} />
+    </div>
+  );
 }
